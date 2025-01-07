dce9ff4064b3f2360c3d728b33691856b4d4c9eb
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-01-06 22:09:48 -0300

    feat(metabase on ecs): postgres rds setup

--- projects/7 - metabase on ecs/modules/sg/rds.tf
+++ projects/7 - metabase on ecs/modules/sg/rds.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "rds" {
 resource "aws_vpc_security_group_ingress_rule" "rds" {
   security_group_id = aws_security_group.rds.id
   referenced_security_group_id = aws_security_group.lb.id
-  from_port         = 3306
-  to_port           = 3306
+  from_port         = 5432
+  to_port           = 5432
   ip_protocol       = "tcp"
 }
 
