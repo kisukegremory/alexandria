e3047aa98a48f500b6579f44f0e70a34dfa307f8
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-05-17 16:46:28 -0300

    feat(efs): is no need inbound rules for ec2 now

--- projects/EFS/network.tf
+++ projects/EFS/network.tf
@@ -8,25 +8,25 @@ resource "aws_security_group" "ec2" {
 }
 
 
-# To Get My Public IP
-data "http" "my_public_ip" {
-  url = "https://ifconfig.co/json"
-  request_headers = {
-    Accept = "application/json"
-  }
-}
+# # To Get My Public IP
+# data "http" "my_public_ip" {
+#   url = "https://ifconfig.co/json"
+#   request_headers = {
+#     Accept = "application/json"
+#   }
+# }
 
-locals {
-  my_public_ip = jsondecode(data.http.my_public_ip.response_body).ip
-}
+# locals {
+#   my_public_ip = jsondecode(data.http.my_public_ip.response_body).ip
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "ec2-ingress" {
-  security_group_id = aws_security_group.ec2.id
-  cidr_ipv4         = "${local.my_public_ip}/32" # My IP
-  ip_protocol       = "tcp"
-  from_port         = 22
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "ec2-ingress" {
+#   security_group_id = aws_security_group.ec2.id
+#   cidr_ipv4         = "${local.my_public_ip}/32" # My IP
+#   ip_protocol       = "tcp"
+#   from_port         = 22
+#   to_port           = 22
+# }
 
 resource "aws_vpc_security_group_egress_rule" "ec2-egress" {
   security_group_id = aws_security_group.ec2.id
