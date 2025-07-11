5699c93103b1b9ee00b608da8479487961927b42
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-07-10 21:56:15 -0300

    feat(S3 Static Site): bucket created

--- projects/S3 Static Site/README.md
+++ projects/S3 Static Site/README.md
@@ -6,6 +6,7 @@
 
 
 Vamos dividir o projeto em 3 etapas:
+*Não é recomendado utilizar backend local, mas para encadear o site para todos, vamos fazer dessa forma*
 1. Prerequirements: Criação de Bucket
 2. Site: Subir index.html, error.html (opcional)
 3. Disponibilizar ao público: Liberar acesso
---
+++ projects/S3 Static Site/bucket/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "local" {
+    path = "./bucket.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      "ManagedBy" = "Terraform"
+    }
+  }
+}
+
+# Bucket needs to be unique
+resource "random_string" "this" {
+  length = 8  
+  min_numeric = 8
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "nina-static-${random_string.this.result}"
+}
+
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
