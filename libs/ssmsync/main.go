73a8ef1cb7ada51341942a9a039fa4a7356648dd
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-08-14 22:10:02 -0300

    feat(ssmsync): reading an .secret

--- libs/ssmsync/main.go
+++ libs/ssmsync/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World! SSM Sync")
+	project := flag.String("project", "", "ssm parameter prefix")
+	flag.Parse()
+	fmt.Println("project: ", *project)
+
+	secrets, _ := os.ReadFile(".secrets")
+
+	fmt.Println("secrets: \n", string(secrets))
 }
