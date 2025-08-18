d7bacf73fc67d3f77cd710954b379c89c1459c47
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-08-18 16:14:41 -0300

    feat(ssmsync): defined pattern as project_name/env_name

--- libs/ssmsync/main.go
+++ libs/ssmsync/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 func (env EnvVar) formatSSMPattern(project string) string {
 	result := fmt.Sprintf("/%s/%s", project, env.Name)
-	result = strings.ReplaceAll(result, "_", "-")
-	result = strings.ToLower(result)
 	return result
+}
+
+func getSSMParameter(ctx context.Context, client *ssm.Client, name string, decrypt bool) (string, error) {
+	// Cria a entrada para a chamada da API GetParameter.
+	input := &ssm.GetParameterInput{
+		Name:           &name,
+		WithDecryption: &decrypt, // Use 'true' para SecureString
+	}
+
+	// Chama a API para obter o parâmetro.
+	result, err := client.GetParameter(ctx, input)
+	if err != nil {
+		return "", fmt.Errorf("falha ao buscar o parâmetro '%s': %w", name, err)
+	}
+
+	// Retorna o valor do parâmetro.
+	return *result.Parameter.Value, nil
 }
 
 func main() {
