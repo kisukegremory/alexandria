4f48a9227564e30d23952dd635318814c61c69cd
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2025-10-27 21:58:20 -0300

    feat(api-gateway): deployment and stage triggers

--- projects/api-gateway/api-gateway.tf
+++ projects/api-gateway/api-gateway.tf
@@ -3,3 +3,23 @@ resource "aws_api_gateway_rest_api" "http_api" {
   name        = local.project_name
   description = "API Gateway com HTTP Proxy para um endpoint externo"
 }
+
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.http_api.id
+  triggers = {
+      redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.external_resource.id,
+      aws_api_gateway_integration.lambda.id
+      ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.http_api.id
+  stage_name    = "dev"
+}
