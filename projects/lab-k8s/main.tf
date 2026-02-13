ef77042ffdbb7e8f137b6ed6634a20b6b63c1f69
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2026-02-13 19:18:59 -0300

    feat(lab-k8s): first version of cluster build on DO

---
+++ projects/lab-k8s/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+locals {
+  project_name = "lab-k8s"
+}
+
+data "digitalocean_kubernetes_versions" "this" {
+
+}
+
+resource "digitalocean_kubernetes_cluster" "this" {
+  name    = "${local.project_name}-cluster"
+  region  = "nyc1"
+  version = data.digitalocean_kubernetes_versions.this.latest_version
+
+  node_pool {
+    name       = "pool-worker-nodes"
+    size       = "s-1vcpu-2gb"
+    node_count = 2
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = digitalocean_kubernetes_cluster.this.kube_config[0].raw_config
+  filename = "${path.module}/kubeconfig-do.yaml"
+}
+
+output "cluster_id" {
+  value = digitalocean_kubernetes_cluster.this.id
+}
